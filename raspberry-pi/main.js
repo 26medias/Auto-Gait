4b175524e888e0b34adc6467f6be1f48337899d8
Julien Loutre <julien@leap-forward.ca>
2024-04-07 09:57:30 -0400

    Update main.js

--- raspberry-pi/main.js
+++ raspberry-pi/main.js
@@ -38,7 +38,7 @@ class CreepyBot {
                             mirrored: true
                         },{
                             anchorAngle: 180-30,
-                            mirrored: false
+                            mirrored: true
                         },{
                             anchorAngle: 240-30,
                             mirrored: true
