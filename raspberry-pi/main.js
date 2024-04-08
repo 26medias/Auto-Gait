fbfee8ff520ce12a19d07f609ad44d84b17148b4
Julien Loutre <julien@leap-forward.ca>
2024-04-08 10:45:13 -0400

    Update main.js

--- raspberry-pi/main.js
+++ raspberry-pi/main.js
@@ -209,7 +209,7 @@ class CreepyBot {
                 x = x - 360;
             }
             out = x * (this.gait.options.leg.mirror[l] ? -1 : 1) - 30;// - (60 * this.gait.options.leg.mirror[l] ? 1 : -1)
-            if (this.gait.options.leg.mirror[l]) {
+            if (!this.gait.options.leg.mirror[l]) {
                 out = 180 - out;
             }
         }
