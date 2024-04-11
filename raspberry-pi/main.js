1bcb730fd55713049364c806ec930c9f5a08f25e
Julien Loutre <julien@leap-forward.ca>
2024-04-10 22:09:07 -0400

    Update main.js

--- raspberry-pi/main.js
+++ raspberry-pi/main.js
@@ -20,7 +20,7 @@ const ServoData = {
 class CreepyBot {
     constructor(options) {
         this.options = _.extend({
-            fps: 10,
+            fps: 30,
             render2D: true,
             render3D: true,
             robot: {
@@ -293,7 +293,7 @@ setTimeout(async () => {
 
     console.log("args", args)
 
-    let bot = new CreepyBot({});
+    let bot = new CreepyBot(args);
     bot.init();
     
     switch (args.op) {
