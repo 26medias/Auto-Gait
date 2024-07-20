c5a0093cdbcdc5c04af848fcb06cbf546d13e3f6
Julien Loutre <julien@leap-forward.ca>
2024-07-19 21:47:52 -0400

    Update main.js

--- raspberry-pi/main.js
+++ raspberry-pi/main.js
@@ -286,11 +286,8 @@ class CreepyBot {
         if (!fixed) {
             return angle;
         }
-        if (n==0) {
-            angle = 180 - angle;
-        }
         if (n==1) {
-            if (!this.gait.options.leg.mirror[l]) {
+            if (this.gait.options.leg.mirror[l]) {
                 angle = 180 - angle;
             }
         }
