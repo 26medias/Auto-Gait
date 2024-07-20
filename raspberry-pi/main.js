c45177a0303e77c5f498c2f712b7d365616ca29e
Julien Loutre <julien@leap-forward.ca>
2024-07-19 21:52:52 -0400

    Update main.js

--- raspberry-pi/main.js
+++ raspberry-pi/main.js
@@ -355,7 +355,7 @@ class CreepyBot {
             //console.log(JSON.stringify([angles[15], angles[16], angles[17], 0, 0, 0]));
             //this.servo2.moveServos(0x07, [angles[15], angles[16], angles[17], 0, 0, 0]);
         }
-        console.log(JSON.stringify(angles));
+        //console.log(JSON.stringify(angles));
     }
     
     render() {
