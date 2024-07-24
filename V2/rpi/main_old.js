0e63e07512e49df8518f48a45d7cac3a4709c4a1
Julien Loutre <julien@leap-forward.ca>
2024-07-23 22:06:32 -0400

    rpi new code

--- V2/rpi/main_old.js
+++ V2/rpi/main_old.js
@@ -1,14 +1,38 @@
 import Robot from '../Robot.js'
 import ServoController from './ServoController.js'
 
+const processArgs = () => {
+    var i;
+    var args 	= process.argv.slice(2);
+    var output 	= {};
+    for (i=0;i<args.length;i++) {
+        var l1	= args[i].substr(0,1);
+        if (l1 == "-") {
+            if (args[i+1] == "true") {
+                args[i+1] = true;
+            }
+            if (args[i+1] == "false") {
+                args[i+1] = false;
+            }
+            if (!isNaN(args[i+1]*1)) {
+                args[i+1] = args[i+1]*1;
+            }
+            output[args[i].substr(1)] = args[i+1];
+            i++;
+        }
+    }
+    return output;
+}
 
 const getServoNum = (leg, n) => {
     return (leg*3) + n;
 }
 
-
 const main = async () => {
     const legAngles = [300, 60, 120, 240];
+
+    const servos = new ServoController();
+    await servos.init();
 
     // Create the robot representation
 	const robot = new Robot({
@@ -40,27 +64,21 @@ const main = async () => {
         },
         onUpdate: function(legIndex, angles) {
             // Whenever an angle changes
-            //console.log("onUpdate", {legIndex, ...angles})
+            console.log("onUpdate", {legIndex, ...angles})
             servos.move(getServoNum(legIndex, 0), angles.shoulder);
             servos.move(getServoNum(legIndex, 1), angles.upper);
             servos.move(getServoNum(legIndex, 2), angles.tip);
         }
     });
 
-    const gait = new Gait(robot, {
-        steps: 20,
-        stepSize: 4,
-        stepHeight: 5,
-        turn: 0 // experimental
-    });
 
-    const gaitFPS = 30;
-    const tick = setInterval(() => {
-        gait.tick();
-    }, 1000/gaitFPS);
-    
+    // Move the tip
+    /*robot.legs[1].tip = robot.legs[1].ik.globalFromRelative({
+        x: 0, y: -7, z: 0
+    })
+    // Apply the changes
+	robot.setAngles(1, robot.legs[1].ik.getAngles());*/
 };
 
 main();
-
 
