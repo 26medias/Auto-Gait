e6422c7dbfdd488fd3f4cd09134ca76b0f5516db
Julien Loutre <julien@leap-forward.ca>
2024-07-24 00:07:44 -0400

    Update main.js

--- V2/rpi/main.js
+++ V2/rpi/main.js
@@ -48,10 +48,10 @@ const main = async () => {
             angle: 0
         },
         angleTweaks: [
-            [-15, 0, 0],
-            [25, 0, 0],
-            [-25, 0, 0],
-            [10, 0, 0]
+            [-15, 30, 0],
+            [25, 30, 0],
+            [-25, 30, 0],
+            [10, 30, 0]
         ],
         onUpdate: function(legIndex, angles) {
             // Whenever an angle changes
