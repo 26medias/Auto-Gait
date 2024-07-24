8fbd6b6e87a532472925422ac2bd204a3eeafb55
Julien Loutre <julien@leap-forward.ca>
2024-07-23 22:15:42 -0400

    Update main.js

--- V2/rpi/main.js
+++ V2/rpi/main.js
@@ -46,7 +46,7 @@ const main = async () => {
             // Whenever an angle changes
             //console.log("onUpdate", {legIndex, ...angles})
             servos.move(getServoNum(legIndex, 0), angles.shoulder);
-            servos.move(getServoNum(legIndex, 1), angles.upper);
+            servos.move(getServoNum(legIndex, 1), 180-angles.upper);
             servos.move(getServoNum(legIndex, 2), angles.tip);
         }
     });
